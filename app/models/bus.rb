9972a6c515c9dee4cc3aa9ee0d8ac9629e73105b
patcantin <patcantin23@gmail.com>
2020-02-10 12:44:50 -0500

    models validations

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,3 +1,8 @@
 class Bus < ApplicationRecord
   belongs_to :user
+
+  validate :description, presence: true
+  validate :price, presence: true
+  validate :capacity, presence: true, inclusion: 1..56
+  validate :name, presence: true
 end
