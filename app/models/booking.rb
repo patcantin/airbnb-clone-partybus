c27157284426057188e25078bc8a1f1cb6b1cb8f
patcantin <patcantin23@gmail.com>
2020-02-11 20:07:32 -0500

    syntax correction on booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,8 +9,9 @@ class Booking < ApplicationRecord
 
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
-      if end_date < start_date
-      errors.date(:end_date,"must be after the start date.")
+
+    if end_date < start_date
+      errors.date(:end_date, "must be after the start date.")
     end
   end
 end
