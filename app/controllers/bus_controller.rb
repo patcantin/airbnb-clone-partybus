4875ce91268d24f430ea0faa8b005d60ef8b7744
patcantin <patcantin23@gmail.com>
2020-02-11 20:24:10 -0500

    bus and booking controllers

---
+++ app/controllers/bus_controller.rb
@@ -0,0 +1,28 @@
+class BusController < ApplicationController
+  before_action :set_bus, only: [:show, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_bus
+    @bus = Bus.find(params[:id])
+  end
+
+  def bus_params
+    params.require(:bus).permit(:name, :capacity, :description, :price)
+  end
+end
