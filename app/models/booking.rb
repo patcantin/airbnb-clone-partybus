49fb98de266932cc391c2cb32dc90dc93e113164
patcantin <patcantin23@gmail.com>
2020-02-11 19:58:48 -0500

    no active record association needed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,7 +9,7 @@ class Booking < ApplicationRecord
 
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
-    if end_date < start_date
+      if end_date < start_date
       errors.date(:end_date,"must be after the start date.")
     end
   end
