03b74d54f75953148ddf2102626f10da2d61f9fe
patcantin <patcantin23@gmail.com>
2020-02-17 14:22:48 -0500

    booking show page to mange (delete) a booking, delete link also added in dashboard page for buses, confirm alert before deleting

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'buses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :buses, only: [:index, :show, :new, :create, :destroy] do
-    resources :bookings, only: %i(new create)
+    resources :bookings, only: %i(show new create destroy)
   end
   get '/pages/dashboard', to: 'pages#dashboard', as: 'dashboard'
 end
